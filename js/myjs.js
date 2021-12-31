eea3a199d44bac2e788b7fbd030d5ab5b2ba8e6c
TandyAlam <tandy.hutama@dwidasa.com>
2021-12-31 13:52:26 +0700

    update portfolio

--- js/myjs.js
+++
@@ -1,8 +0,0 @@
-const sideNav = document.querySelectorAll('.sidenav');
-M.Sidenav.init(sideNav);
-
-const scrollSpy = document.querySelectorAll('.scrollspy');
-M.ScrollSpy.init(scrollSpy, {
-    scrollOffset: 70,
-    throttle: 5000
-});
